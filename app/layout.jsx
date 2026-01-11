62085ed31f8a8153ff7e03a29fc16fabf9ee8cb1
unknown <kasagojara@gmail.com>
2026-01-11 19:47:41 +0530

    Layout.jsx fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import Navbar from "../components/navbar";
 import ThemeToggle from "../components/theme-toggle";
 import Footer from "../components/footer";
@@ -12,20 +13,24 @@ import ToastContainer from "../components/toast-container";
 export const metadata = {
   title: "Sathya Hardwares",
   description: "Hardware shop website",
+  other: {
+    "monetag": "675a111ad604a704dab475c0a57f5fd0"
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://quge5.com/88/tag.min.js" data-zone="200963" async data-cfasync="false">
-        </script>
-      </head>
-      <head>
-        <meta name="monetag" content="675a111ad604a704dab475c0a57f5fd0">
-        </meta>
+        <meta name="monetag" content="675a111ad604a704dab475c0a57f5fd0" />
       </head>
       <body>
+        <Script 
+          src="https://quge5.com/88/tag.min.js"
+          data-zone="200963"
+          strategy="afterInteractive"
+          data-cfasync="false"
+        />
         <ThemeProvider>
           <ToastProvider>
             <TranslationProvider>
