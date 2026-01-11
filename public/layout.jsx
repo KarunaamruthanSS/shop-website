e2c4c8e85c57884dfb63aded75cf7cb412dd01ba
KarunaamruthanSS <kasagojara@gmail.com>
2026-01-11 18:17:26 +0530

    Add files via upload

---
+++ public/layout.jsx
@@ -0,0 +1,49 @@
+import "./globals.css";
+import Navbar from "../components/navbar";
+import ThemeToggle from "../components/theme-toggle";
+import Footer from "../components/footer";
+import { CartProvider } from "../lib/cartContext";
+import { TranslationProvider } from "../lib/translationContext";
+import { ToastProvider } from "../lib/toastContext";
+import { SessionProvider } from "../lib/sessionContext";
+import { ThemeProvider } from "../lib/themeContext";
+import ToastContainer from "../components/toast-container";
+
+export const metadata = {
+  title: "Sathya Hardwares",
+  description: "Hardware shop website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6241972269720165"
+            crossorigin="anonymous">
+
+        </script>
+      </head>
+      <head>
+        <meta name="monetag" content="675a111ad604a704dab475c0a57f5fd0">
+        </meta>
+      </head>
+      <body>
+        <ThemeProvider>
+          <ToastProvider>
+            <TranslationProvider>
+              <SessionProvider>
+                <CartProvider>
+                  <Navbar />
+                  <ThemeToggle />
+                  {children}
+                  <Footer />
+                  <ToastContainer />
+                </CartProvider>
+              </SessionProvider>
+            </TranslationProvider>
+          </ToastProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
